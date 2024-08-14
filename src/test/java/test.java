756725ed415f78cda2124b5ca09834373ac8d68f
Andrei Olkhovskii <andrejolhovskij@MacBook-Pro-Andrej.local>
2024-08-15 00:01:15 +0300

    complete test

---
+++ src/test/java/test.java
@@ -0,0 +1,47 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class test {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = "chrome";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+    }
+
+    private static void executeJavaScript(String s) {
+    }
+
+    @Test
+    void searchJUnit5() {
+
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".markdown-body").find(byText("Soft assertions")).shouldHave(text("Soft assertions")).click();
+        $("#wiki-body").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                    @Test
+                    void test() {
+                        Configuration.assertionMode = SOFT;
+                        open("page.html");
+
+                        $("#first").should(visible).click();
+                        $("#second").should(visible).click();
+                    }
+                }
+                """));
+
+    }
+}
